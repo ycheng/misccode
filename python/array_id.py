83d7e636b33c02c4ec92ee2122955667beaaf475
Yuan-Chen Cheng <yc.cheng@canonical.com>
2015-09-21 15:46:03 +0800

    id for non-mutalbe and mutable variable regarding to passed to function

--- python/array_id.py
+++ python/array_id.py
@@ -3,9 +3,9 @@
 #   use reference for mutable value (object)
 def foo(a):
   print (id(a))
-  a += 1 # or a = 1
+  a.append(1) # or a = 1
   print (id(a))
-a = 0
+a = [0]
 print (id(a))
 foo(a)
 print (a)
