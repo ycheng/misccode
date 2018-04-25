36b81f33c6273b63c4e5f874a73e74f9e0454191
Yuan-Chen Cheng <yc.cheng@canonical.com>
2018-04-25 23:15:54 +0800

    sample

---
+++ ml/matplot-001.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+plt.plot([0, 1, 4, 9, 16, 25])
+plt.ylabel('y axis, equal to x square')
+plt.ylabel('x axis')
+plt.show()
---
+++ ml/numpy_pandas.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+import numpy as np
+
+def line():
+    print("-" * 50)
+
+ar = np.array([1,2,3])
+print(ar)
+print(2 * ar)
+print(ar * 2)
+
+line()
+
+s1 = pd.Series([1,2,3, np.nan, 44, 1])
+print(type(s1))
+print(s1)
+print(s1 * 3)
+print(3 * s1)
+
+line()
+
+s2 = pd.Series(['4 cups', '1 cpu', '2 large', '1 can'], index=['Flour', 'Milk', 'Eggs', 'Spam'])
+print(s2)
+
+line()
+
+q1_df = pd.DataFrame({'Apples': [30], 'Bananas':[21]})
+print(q1_df)
+
+line()
+
+q1_df = pd.DataFrame({'Apples': [30, 41], 'Bananas':[21, 34]}, index=['2017 Sales', '2018 Sales'])
+print(q1_df)
+
+line()
+
+q2_df = pd.DataFrame([[30, 21], [41, 34]], index=['2017 Sales', '2018 Sales'], columns=['Apples', 'Bananas'])
+print(q2_df)
