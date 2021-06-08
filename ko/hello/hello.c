936ec574b26da637c1a91c06b3e0db0ee597d1f3
Yuan-Chen Cheng <yc.cheng@canonical.com>
2021-06-08 19:08:38 +0800

    add alias related sampe code

--- ko/hello/hello.c
+++ ko/hello/hello.c
@@ -5,31 +5,48 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
-// #include <linux/init.h>
+#include <linux/init.h>
+#include <linux/pci.h>
 
 MODULE_DESCRIPTION("Hello World !!");
 MODULE_AUTHOR("Bo-Yi Wu ");
 MODULE_LICENSE("GPL");
 
 static int __init hello_init(void){
-    printk("This is hello_module, welcome to Linux kernel \n");
+    printk(KERN_INFO "This is hello_module, welcome to Linux kernel \n");
     // pr_info("The process is \"%s\" (pid %i)\n", current->comm, current->pid);
     return 0; 
 }
 
 static void __exit hello_exit(void){
-    printk("see you next time!\n"); 
+    printk(KERN_INFO "see you next time!\n"); 
 }
+
+
+struct pci_device_id nv_pci_table[] = {
+    {
+        .vendor      = PCI_VENDOR_ID_NVIDIA,
+        .device      = PCI_ANY_ID,
+        .subvendor   = PCI_ANY_ID,
+        .subdevice   = PCI_ANY_ID,
+        .class       = (PCI_CLASS_DISPLAY_VGA << 8),
+        .class_mask  = ~0
+    },
+    { }
+};
 
 module_init(hello_init);
 module_exit(hello_exit);
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mr Q");
 MODULE_DESCRIPTION("hello kernel module");
-MODULE_ALIAS("hello");
 
+// alias: define directly
+// MODULE_ALIAS("hello"); 
 
-
+// alias: define by macro
+MODULE_DEVICE_TABLE(pci, nv_pci_table); 
 
 // static int __init hello_init(void)
 // {
@@ -37,11 +54,3 @@ MODULE_ALIAS("hello");
 //     pr_info("The process is \"%s\" (pid %i)\n", current->comm, current->pid);
 //     return 0;
 // }
-
-// static void __exit hello_exit(void)
-// {
-//     printk(KERN_INFO "Goodbye\n");
-// }
-
-// module_init(hello_init);
-// module_exit(hello_exit);
